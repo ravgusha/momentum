1c05b3a85e8beef014c4afeb679847e92293b940
ravgusha <ravgusha@gmail.com>
2021-09-21 14:05:46 +0300

    refactor: cgange slider logic

--- museum/script.js
+++ museum/script.js
@@ -5,53 +5,47 @@ let nextBtn = document.getElementById("next");
 let counter = document.getElementById("counter");
 let currentSlide = document.getElementById("currentSlide");
 let paginationSquare = document.getElementsByClassName("square");
+// let squares = document.getElementById("squares");
 
+let currentIndex = 0;
+let newIndex = 0;
 
-let i = 0;
+function showCurrentSlide() { // Отображаем номер слайда в дробной пагинации
+  currentSlide.textContent = '0' + (newIndex + 1);
+};
 
-console.log(currentSlide);
-// function getImage() {
-
-//   slide[0].classList.add("active"); // Отображаем первый слайд сразу при загрузке страницы
-// }
-
-// getImage();
-
-
-
-prevBtn.addEventListener('click', () => {
-  slide[i].classList.remove("active");
-  paginationSquare[i].classList.remove("active")
-  i--;
-
-  if (i < 0) {
-    i = slide.length - 1; // При нажатии на стрелку влево на 1 слайде показываем посл.слайд
+squares.onclick = function(e) { // Квадратики пагинации
+  let target = e.target;
+  if (target.classList.contains("square")) {
+     newIndex = Number(target.getAttribute("data-index"));
+     console.log(newIndex);
+     navigateSlider();
   }
-
-  slide[i].classList.add("active");
-  paginationSquare[i].classList.add("active");
-  currentSlideShow();
-});
-
-
-nextBtn.addEventListener('click', () => {
-  slide[i].classList.remove("active");
-  paginationSquare[i].classList.remove("active")
-  i++;
-
-  if (i >= slide.length) {
-    i = 0; // При нажатии на стрелку вправо на послед. слайде показываем 1 слайд
-  }
-
-  slide[i].classList.add("active");
-  paginationSquare[i].classList.add("active");
-  currentSlideShow();
-});
-
-
-function currentSlideShow() {
-
-  currentSlide.textContent = '0' + (i+1);
-
 }
 
+prevBtn.onclick = function() { // Стрелка влево
+  newIndex--;
+  navigateSlider();
+  showCurrentSlide();
+}
+
+nextBtn.onclick = function() { // Стрелка вправо
+  newIndex++;
+  navigateSlider();
+  showCurrentSlide();
+}
+
+function navigateSlider() {
+  if (newIndex === -1) newIndex = slide.length - 1;
+  else if (newIndex === slide.length) newIndex = 0;
+
+
+  slide[currentIndex].classList.remove("active");
+  slide[newIndex].classList.add("active");
+
+  paginationSquare[currentIndex].classList.remove('active');
+  paginationSquare[newIndex].classList.add('active');
+
+  currentIndex = newIndex;
+  showCurrentSlide();
+}
