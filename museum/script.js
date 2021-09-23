2d222f09593ec550e58100305ff6f8ff89a4563e
ravgusha <ravgusha@gmail.com>
2021-09-23 13:19:59 +0300

    refactor: move player controls

--- museum/script.js
+++ museum/script.js
@@ -133,3 +133,81 @@ function initComparisons() {
     }
   }
 }
+
+initComparisons();
+
+
+// ВИДЕО-ПЛЕЕР
+
+/* Get Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const controls = player.querySelector(".player__controls");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggleButton = player.querySelector(".togglePlayback");
+const volume = player.querySelector(".playerVolume");
+const fullscreen = player.querySelector(".toggleFullscreen");
+const circle = document.querySelector(".player__playbackCircle");
+
+/* Functions */
+function togglePlay() {
+ const icon = toggleButton.querySelector(".player__playbackIcon");
+
+ video.paused ? video.play() : video.pause();
+
+ icon.classList.toggle("player__playbackIcon--paused");
+ circle.classList.toggle("player__playbackCircle--paused");
+
+
+
+}
+
+function handleRangeUpdate() {
+ video[this.name] = this.value;
+}
+
+function handleProgress() {
+ const percent = video.currentTime / video.duration * 100;
+ progressBar.style.flexBasis = `${percent}%`;
+}
+
+function handleSeek(e) {
+ const seekTime = e.offsetX / progress.offsetWidth * video.duration;
+ video.currentTime = seekTime;
+}
+
+// Create fullscreen video button
+function toggleFullscreen() {
+ if (!document.webkitFullscreenElement) {
+  if (video.requestFullScreen) {
+   player.requestFullScreen();
+  } else if (video.webkitRequestFullScreen) {
+   player.webkitRequestFullScreen();
+  } else if (video.mozRequestFullScreen) {
+   player.mozRequestFullScreen();
+  }
+ } else {
+  document.webkitExitFullscreen();
+ }
+}
+
+/* Hook up the event listeners */
+
+video.addEventListener("click", togglePlay);
+circle.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", handleProgress);
+
+toggleButton.addEventListener("click", togglePlay);
+volume.addEventListener("change", handleRangeUpdate);
+volume.addEventListener("mousemove", handleRangeUpdate);
+
+let mousedown = false;
+progress.addEventListener("click", handleSeek);
+progress.addEventListener("mousemove", e => mousedown && handleSeek(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
+
+fullscreen.addEventListener("click", toggleFullscreen);
+video.addEventListener("dblclick", toggleFullscreen);
+
