dced8804ef61c38231d0d546e96759c531ff8791
ravgusha <62184992+ravgusha@users.noreply.github.com>
2021-10-21 20:11:37 +0300

    feat: add quotes and audio player blocks

---
+++ momentum/js/playList.js
@@ -0,0 +1,18 @@
+let playList = [{
+    name: "Charming Smile - Egor Grushin",
+    file: "../assets/sounds/track1.mp3",
+    duration: "03:06"
+},
+{
+    name: "Inspiration - Florian Christl",
+    file: "../assets/sounds/track2.mp3",
+    duration: "02:13"
+},
+{
+    name: "Fly - Ludovico Einaudi",
+    file: "../assets/sounds/track3.mp3",
+    duration: "04:38"
+}
+]
+
+export default playList;
--- momentum/js/script.js
+++ momentum/js/script.js
@@ -79,6 +79,7 @@ console.log(timeOfDay)
 
 
 // USER NAME SAVING
+
 userName.addEventListener('input', saveName);
 
 function saveName() {
@@ -87,16 +88,18 @@ function saveName() {
 
 userName.value = localStorage.getItem('name');
 
+// GETTING RANDOM NUMBER 
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 // GETTING BACKGROUND IMAGE 
 
 let random;
 
-function getBackground() {
 
-    function randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+function getBackground() {
 
     random = randomIntFromInterval(1, 20)
     if (random < 10) {
@@ -199,4 +202,289 @@ async function getWeather() {
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// GET QUOTES 
 
+let quote = document.querySelector('.quote');
+let author = document.querySelector('.author');
+let quoteBtn = document.querySelector('.change-quote')
+
+async function getQuotes() {
+    const quotes = '../js/data/quotes-en.json';
+
+    const res = await fetch(quotes);
+    const data = await res.json();
+
+    let randomQuote = randomIntFromInterval(1, 102);
+
+    quote.textContent = `${data.quotes[randomQuote].quote}`;
+    author.textContent = `${data.quotes[randomQuote].author}`;
+}
+document.addEventListener('DOMContentLoaded', getQuotes);
+
+quoteBtn.addEventListener('click', getQuotes);
+
+
+// PLAYER 
+
+function createTrackItem(index, name, duration) {
+    var trackItem = document.createElement('div');
+    trackItem.setAttribute("class", "playlist-track-ctn");
+    trackItem.setAttribute("id", "ptc-" + index);
+    trackItem.setAttribute("data-index", index);
+    document.querySelector(".playlist-ctn").appendChild(trackItem);
+
+    var playBtnItem = document.createElement('div');
+    playBtnItem.setAttribute("class", "playlist-btn-play");
+    playBtnItem.setAttribute("id", "pbp-" + index);
+    document.querySelector("#ptc-" + index).appendChild(playBtnItem);
+
+    var btnImg = document.createElement('i');
+    btnImg.setAttribute("class", "fas fa-play");
+    btnImg.setAttribute("height", "40");
+    btnImg.setAttribute("width", "40");
+    btnImg.setAttribute("id", "p-img-" + index);
+    document.querySelector("#pbp-" + index).appendChild(btnImg);
+
+    var trackInfoItem = document.createElement('div');
+    trackInfoItem.setAttribute("class", "playlist-info-track");
+    trackInfoItem.innerHTML = name
+    document.querySelector("#ptc-" + index).appendChild(trackInfoItem);
+
+    var trackDurationItem = document.createElement('div');
+    trackDurationItem.setAttribute("class", "playlist-duration");
+    trackDurationItem.innerHTML = duration
+    document.querySelector("#ptc-" + index).appendChild(trackDurationItem);
+}
+
+var listAudio = [{
+        name: "Charming Smile - Egor Grushin",
+        file: "../assets/sounds/track1.mp3",
+        duration: "03:06"
+    },
+    {
+        name: "Inspiration - Florian Christl",
+        file: "../assets/sounds/track2.mp3",
+        duration: "02:13"
+    },
+    {
+        name: "Fly - Ludovico Einaudi",
+        file: "../assets/sounds/track3.mp3",
+        duration: "04:38"
+    }
+]
+
+for (var i = 0; i < listAudio.length; i++) {
+    createTrackItem(i, listAudio[i].name, listAudio[i].duration);
+}
+var indexAudio = 0;
+
+function loadNewTrack(index) {
+    var player = document.querySelector('#source-audio')
+    player.src = listAudio[index].file
+    document.querySelector('.title').innerHTML = listAudio[index].name
+    this.currentAudio = document.getElementById("myAudio");
+    this.currentAudio.load()
+    this.toggleAudio()
+    this.updateStylePlaylist(this.indexAudio, index)
+    this.indexAudio = index;
+}
+
+var playListItems = document.querySelectorAll(".playlist-track-ctn");
+
+for (let i = 0; i < playListItems.length; i++) {
+    playListItems[i].addEventListener("click", getClickedElement.bind(this));
+}
+
+function getClickedElement(event) {
+    for (let i = 0; i < playListItems.length; i++) {
+        if (playListItems[i] == event.target) {
+            var clickedIndex = event.target.getAttribute("data-index")
+            if (clickedIndex == this.indexAudio) { // alert('Same audio');
+                this.toggleAudio()
+            } else {
+                loadNewTrack(clickedIndex);
+                console.log(clickedIndex);
+            }
+
+        }
+    }
+}
+
+document.querySelector('#source-audio').src = listAudio[indexAudio].file
+document.querySelector('.title').innerHTML = listAudio[indexAudio].name
+
+
+var currentAudio = document.getElementById("myAudio");
+
+currentAudio.load();
+
+currentAudio.onloadedmetadata = function () {
+    document.getElementsByClassName('duration')[0].innerHTML = this.getMinutes(this.currentAudio.duration)
+}.bind(this);
+
+var interval1;
+
+function toggleAudio() {
+
+    if (this.currentAudio.paused) {
+        document.querySelector('#icon-play').style.display = 'none';
+        document.querySelector('#icon-pause').style.display = 'block';
+        document.querySelector('#ptc-' + this.indexAudio).classList.add("active-track");
+        this.playToPause(this.indexAudio)
+        this.currentAudio.play();
+    } else {
+        document.querySelector('#icon-play').style.display = 'block';
+        document.querySelector('#icon-pause').style.display = 'none';
+        this.pauseToPlay(this.indexAudio)
+        this.currentAudio.pause();
+    }
+}
+
+function pauseAudio() {
+    this.currentAudio.pause();
+    clearInterval(interval1);
+}
+
+var timer = document.getElementsByClassName('timer')[0]
+
+var barProgress = document.getElementById("myBar");
+
+
+var width = 0;
+
+function onTimeUpdate() {
+    var t = this.currentAudio.currentTime
+    timer.innerHTML = this.getMinutes(t);
+    this.setBarProgress();
+    if (this.currentAudio.ended) {
+        document.querySelector('#icon-play').style.display = 'block';
+        document.querySelector('#icon-pause').style.display = 'none';
+        this.pauseToPlay(this.indexAudio)
+        if (this.indexAudio < listAudio.length - 1) {
+            var index = parseInt(this.indexAudio) + 1
+            this.loadNewTrack(index)
+        }
+    }
+}
+
+
+function setBarProgress() {
+    var progress = (this.currentAudio.currentTime / this.currentAudio.duration) * 100;
+    document.getElementById("myBar").style.width = progress + "%";
+}
+
+
+function getMinutes(t) {
+    var min = parseInt(parseInt(t) / 60);
+    var sec = parseInt(t % 60);
+    if (sec < 10) {
+        sec = "0" + sec
+    }
+    if (min < 10) {
+        min = "0" + min
+    }
+    return min + ":" + sec
+}
+
+var progressbar = document.querySelector('#myProgress')
+progressbar.addEventListener("click", seek.bind(this));
+
+
+function seek(event) {
+    var percent = event.offsetX / progressbar.offsetWidth;
+    this.currentAudio.currentTime = percent * this.currentAudio.duration;
+    barProgress.style.width = percent * 100 + "%";
+}
+
+function forward() {
+    this.currentAudio.currentTime = this.currentAudio.currentTime + 5
+    this.setBarProgress();
+}
+
+function rewind() {
+    this.currentAudio.currentTime = this.currentAudio.currentTime - 5
+    this.setBarProgress();
+}
+
+
+function next() {
+    if (this.indexAudio < listAudio.length - 1) {
+        var oldIndex = this.indexAudio
+        this.indexAudio++;
+        updateStylePlaylist(oldIndex, this.indexAudio)
+        this.loadNewTrack(this.indexAudio);
+
+    } else if (this.indexAudio = listAudio.length - 1) {
+
+        var oldIndex = this.indexAudio;
+        this.indexAudio = 0;
+        updateStylePlaylist(oldIndex, this.indexAudio)
+        this.loadNewTrack(this.indexAudio);
+    }
+}
+
+function previous() {
+    if (this.indexAudio > 0) {
+        var oldIndex = this.indexAudio
+        this.indexAudio--;
+        updateStylePlaylist(oldIndex, this.indexAudio)
+        this.loadNewTrack(this.indexAudio);
+    } else if (this.indexAudio <= 0) {
+        var oldIndex = this.indexAudio;
+        this.indexAudio = listAudio.length - 1;
+        updateStylePlaylist(oldIndex, this.indexAudio)
+        this.loadNewTrack(this.indexAudio);
+    }
+}
+
+function updateStylePlaylist(oldIndex, newIndex) {
+    document.querySelector('#ptc-' + oldIndex).classList.remove("active-track");
+    this.pauseToPlay(oldIndex);
+    document.querySelector('#ptc-' + newIndex).classList.add("active-track");
+    this.playToPause(newIndex)
+}
+
+function playToPause(index) {
+    var ele = document.querySelector('#p-img-' + index)
+    ele.classList.remove("fa-play");
+    ele.classList.add("fa-pause");
+}
+
+function pauseToPlay(index) {
+    var ele = document.querySelector('#p-img-' + index)
+    ele.classList.remove("fa-pause");
+    ele.classList.add("fa-play");
+}
+
+let btnMute = document.querySelector('#toggleMute');
+let volUp = document.querySelector('#icon-vol-up');
+let volMute = document.querySelector('#icon-vol-mute');
+let volumeRange = document.getElementById('volumeRange');
+
+function toggleMute() {
+
+    if (this.currentAudio.muted == false) {
+        this.currentAudio.muted = true
+        volUp.style.display = "none"
+        volMute.style.display = "block"
+    } else {
+        this.currentAudio.muted = false
+        volMute.style.display = "none"
+        volUp.style.display = "block"
+    }
+}
+
+
+setVolume = function () {
+    currentAudio.volume = document.getElementById("volumeRange").value;
+    if (document.getElementById("volumeRange").value <= 0.02) {
+        this.currentAudio.muted = true;
+        volUp.style.display = "none";
+        volMute.style.display = "block";
+    } else if (document.getElementById("volumeRange").value > 0.02) {
+        this.currentAudio.muted = false;
+        volUp.style.display = "block";
+        volMute.style.display = "none";
+    }
+
+};
